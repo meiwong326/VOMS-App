c095739a5ab7caca7746809c158e5439283b025b
meiwong326 <50881017+meiwong326@users.noreply.github.com>
2020-03-17 21:47:20 -0400

    Tried to get speed

--- app/src/main/java/com/example/voms/VMSTest.java
+++ app/src/main/java/com/example/voms/VMSTest.java
@@ -1,20 +1,27 @@
 package com.example.voms;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.location.Location;
+import android.widget.Toast;
 
 
 public class VMSTest extends AppCompatActivity implements SensorEventListener{
@@ -22,11 +29,14 @@ public class VMSTest extends AppCompatActivity implements SensorEventListener{
     private MediaPlayer metronome;
 
     private Button button;
-
     private TextView azimuthText;
-
+    private TextView speedText;
     private ImageView left_arrow;
     private ImageView right_arrow;
+
+    private Location mLastLocation;
+    private LocationManager locationManager;
+    private LocationProvider locationProvider;
 
     private SensorManager sensorManager;
     private Sensor sensorAccelerometer;
@@ -37,7 +47,6 @@ public class VMSTest extends AppCompatActivity implements SensorEventListener{
     private float[] rotationMatrix = new float[9];
     private float[] remapMatrix = new float[9];
     private float[] orientationValues = new float[3];
-
     float azimuth;
     double azimuthDegrees;
 
@@ -58,10 +67,25 @@ public class VMSTest extends AppCompatActivity implements SensorEventListener{
         });
 
         azimuthText = findViewById(R.id.degreesRotated);
+        speedText = findViewById(R.id.speed);
         left_arrow = findViewById(R.id.arrow_left);
         right_arrow = findViewById(R.id.arrow_right);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        /*
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getBaseContext(), "First enable LOCATION ACCESS in settings.", Toast.LENGTH_LONG).show();
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, locationListener);
+        //isLocationEnabled();
+        locationProvider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+        */
+
+        sensorManager = (SensorManager) getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
         sensorAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorMagnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
@@ -89,14 +113,12 @@ public class VMSTest extends AppCompatActivity implements SensorEventListener{
         metronome.stop();
     }
 
-    /**
-     * Must be implemented to satisfy the SensorEventListener interface;
-     * unused in this app.
-     */
+    @Override
     public final void onAccuracyChanged(Sensor sensor, int accuracy) {
         // Do something here if sensor accuracy changes.
     }
 
+    @Override
     public void onSensorChanged(SensorEvent event) {
         int sensorType = event.sensor.getType();
 
@@ -125,18 +147,58 @@ public class VMSTest extends AppCompatActivity implements SensorEventListener{
 
             //azimuthText.setText(String.format("%1.2f", azimuthDegrees));
 
+            // azimuth degrees = -1 * y-rotation in virtual sensors
+
             // If the user has turned 75 degrees to the left, display right arrow
-            if (azimuthDegrees > 75) {
+            if (azimuthDegrees < -75) {
                 left_arrow.setVisibility(View.INVISIBLE);
                 right_arrow.setVisibility(View.VISIBLE);
             }
             // If the user has turned 75 degrees to the right, display left arrow
-            if (azimuthDegrees < -75) {
+            if (azimuthDegrees > 75) {
                 right_arrow.setVisibility(View.INVISIBLE);
                 left_arrow.setVisibility(View.VISIBLE);
             }
         }
 
     }
+
+    /*
+     LocationListener locationListener = new LocationListener() {
+        private Location mLastLocation;
+
+        @Override
+        public void onLocationChanged(Location pCurrentLocation) {
+            //manually calculate speed
+            double speed = 0;
+            if (this.mLastLocation != null)
+                speed = Math.sqrt(
+                        Math.pow(pCurrentLocation.getLongitude() - mLastLocation.getLongitude(), 2)
+                                + Math.pow(pCurrentLocation.getLatitude() - mLastLocation.getLatitude(), 2)
+                ) / (pCurrentLocation.getTime() - this.mLastLocation.getTime());
+            //if there is speed from location
+            if (pCurrentLocation.hasSpeed())
+                //get location speed
+                speed = pCurrentLocation.getSpeed();
+            this.mLastLocation = pCurrentLocation;
+            speedText.setText(String.format("%1.2f", speed));
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+     */
 
 }
