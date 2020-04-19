b5decfc3a520048b85939b73aaedd5e3f0a573cc
meiwong326 <d881017+meiwong326@users.noreply.github.com>
2020-04-19 10:59:23 -0400

    Convergence: Face Detection

--- app/src/main/java/com/example/voms/Convergence.java
+++ app/src/main/java/com/example/voms/Convergence.java
@@ -50,8 +50,10 @@ import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -59,9 +61,17 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Convergence extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+public class Convergence extends AppCompatActivity /*implements CameraBridgeViewBase.CvCameraViewListener2*/ {
 
     private static final String TAG = "OpenCVConfiguration";
+
+    private static final int JAVA_DETECTOR = 0;
+    private static final int TM_SQDIFF = 0;
+    private static final int TM_SQDIFF_NORMED = 1;
+    private static final int TM_CCOEFF = 2;
+    private static final int TM_CCOEFF_NORMED = 3;
+    private static final int TM_CCORR = 4;
+    private static final int TM_CCORR_NORMED = 5;
 
     private Button button;
     TextView  distanceText;
@@ -73,50 +83,92 @@ public class Convergence extends AppCompatActivity implements CameraBridgeViewBa
 
     JavaCameraView javaCameraView;
     File cascadeFile;
+    File cascadeFileEye;
     CascadeClassifier faceDetector;
+    CascadeClassifier eyeDetector;
 
+    int learn_frames = 0;
+    int method = 0;
+    int detectorType = JAVA_DETECTOR;
+    int absoluteFaceSize = 0;
+    float relativeFaceSize = 0.2f;
+    String[] detectorName;
+    Mat mRgba, mGray;
+    Mat teplateR, teplateL;
 
-
-    Mat mRgba, mRgbaT, mRgbaF, mGray, mGrayT, mGrayF;
-
-    BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
+   /* BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) throws IOException {
             switch (status) {
                 case BaseLoaderCallback.SUCCESS: {
                     javaCameraView.enableView();
 
-                    InputStream inputStream = getResources().openRawResource(R.raw.haarcascade_frontalface_alt2);
-                    File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
-                    cascadeFile = new File(cascadeDir, "haarcascade_frontalface_alt2.xml");
+                    try {
+                        InputStream inputStream = getResources().openRawResource(R.raw.lbpcascade_frontalface);
+                        File cascadeDir = getDir("cascade", Context.MODE_PRIVATE);
+                        cascadeFile = new File(cascadeDir, "lbpcascade_frontalface.xml");
+                        FileOutputStream fileOutputStream = new FileOutputStream(cascadeFile);
 
-                    FileOutputStream fileOutputStream = new FileOutputStream(cascadeFile);
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = inputStream.read(buffer)) != -1) {
+                            fileOutputStream.write(buffer, 0, bytesRead);
+                        }
+                        inputStream.close();
+                        fileOutputStream.close();
 
-                    byte[] buffer = new byte[4096];
-                    int bytesRead;
+                        *//*InputStream inputStreamEye = getResources().openRawResource(R.raw.haarcascade_lefteye_2splits);
+                        File cascadeDirEye = getDir("cascadeEye", Context.MODE_PRIVATE);
+                        cascadeFileEye = new File(cascadeDirEye, "haarcascade_lefteye_2splits.xml");
+                        FileOutputStream fileOutputStreamEye = new FileOutputStream(cascadeDirEye);
 
-                    while((bytesRead = inputStream.read(buffer)) != -1) {
-                        fileOutputStream.write(buffer, 0, bytesRead);
+                        byte[] bufferEye = new byte[4096];
+                        int bytesReadEye;
+                        while ((bytesReadEye = inputStreamEye.read(bufferEye)) != -1) {
+                            fileOutputStreamEye.write(bufferEye, 0, bytesReadEye);
+                        }
+                        inputStreamEye.close();
+                        fileOutputStreamEye.close();
+                         *//*
+
+                        faceDetector = new CascadeClassifier(
+                                cascadeFile.getAbsolutePath());
+                        if (faceDetector.empty()) {
+                            Log.e(TAG, "Failed to load cascade classifier");
+                            faceDetector = null;
+                        } else
+                            Log.i(TAG, "Loaded cascade classifier from "
+                                    + cascadeFile.getAbsolutePath());
+
+                        *//*eyeDetector = new CascadeClassifier(
+                                cascadeFileEye.getAbsolutePath());
+                        if (eyeDetector.empty()) {
+                            Log.e(TAG, "Failed to load cascade classifier");
+                            eyeDetector = null;
+                        } else
+                            Log.i(TAG, "Loaded cascade classifier from "
+                                    + cascadeFile.getAbsolutePath());
+                         *//*
+
+                        cascadeDir.delete();
+                        //cascadeFileEye.delete();
+                    }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                        Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
                     }
 
-                    inputStream.close();
-                    fileOutputStream.close();
-
-                    faceDetector = new CascadeClassifier(cascadeFile.getAbsolutePath());
-
-                    if (faceDetector.empty())
-                        faceDetector = null;
-                    else
-                        cascadeDir.delete();
-
-                    break;
+                    javaCameraView.enableFpsMeter();
                 }
+                break;
                 default: {
                     super.onManagerConnected(status);
                 }
+                break;
             }
         }
-    };
+    };*/
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -151,9 +203,9 @@ public class Convergence extends AppCompatActivity implements CameraBridgeViewBa
             createCameraSource();
         }
 
-        javaCameraView = findViewById(R.id.camera_view);
+/*        javaCameraView = findViewById(R.id.camera_view);
         javaCameraView.setVisibility(SurfaceView.VISIBLE);
-        javaCameraView.setCvCameraViewListener(this);
+        javaCameraView.setCvCameraViewListener(this);*/
     }
 
     private Camera frontCam() {
@@ -253,7 +305,7 @@ public class Convergence extends AppCompatActivity implements CameraBridgeViewBa
         });
     }
 
-    @Override
+    /*@Override
     public void onCameraViewStarted(int width, int height) {
         mRgba = new Mat(height, width, CvType.CV_8UC4);
         mGray = new Mat(height, width, CvType.CV_8UC4);
@@ -266,33 +318,144 @@ public class Convergence extends AppCompatActivity implements CameraBridgeViewBa
     }
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        //rotate clockwise
-        Core.flip(inputFrame.rgba().t(), mRgba,1);
-        Core.flip(inputFrame.gray().t(), mGray,1);
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
 
-        mRgba = Feature_DetectionNATIVE(mRgba,mGray);
-
-        //rotate counter clockwise
-        Core.flip(mRgba.t(), mRgba,0);
-
-        return mRgba;
-    }
-
-    public Mat Feature_DetectionNATIVE(Mat mRgba, final Mat Gray) {
         MatOfRect faceDetections = new MatOfRect();
-        faceDetector.detectMultiScale(mRgba, faceDetections);
+        if (faceDetector != null)
+            faceDetector.detectMultiScale(mRgba, faceDetections);
 
-        for (Rect rect: faceDetections.toArray()) {
-            Imgproc.rectangle(mRgba, new Point(rect.x, rect.y),
-                    new Point(rect.x + rect.width, rect.y + rect.height),
+        for (Rect r : faceDetections.toArray()) {
+            Imgproc.rectangle(mRgba, new Point(r.x, r.y),
+                    new Point(r.x + r.width, r.y + r.height),
                     new Scalar(255, 0, 0));
+
+            Rect eyearea = new Rect(r.x + r.width / 8,
+                    (int) (r.y + (r.height / 4.5)), r.width - 2 * r.width / 8,
+                    (int) (r.height / 3.0));
+
+            Rect eyearea_right = new Rect(r.x + r.width / 16,
+                    (int) (r.y + (r.height / 4.5)),
+                    (r.width - 2 * r.width / 16) / 2, (int) (r.height / 3.0));
+            Rect eyearea_left = new Rect(r.x + r.width / 16
+                    + (r.width - 2 * r.width / 16) / 2,
+                    (int) (r.y + (r.height / 4.5)),
+                    (r.width - 2 * r.width / 16) / 2, (int) (r.height / 3.0));
+
+            Imgproc.rectangle(mRgba, eyearea_left.tl(), eyearea_left.br(),
+                    new Scalar(255, 0, 0), 2);
+            Imgproc.rectangle(mRgba, eyearea_right.tl(), eyearea_right.br(),
+                    new Scalar(255, 0, 0), 2);
+
+            *//* if (learn_frames < 5) {
+                teplateR = get_template(eyeDetector, eyearea_right, 24);
+                teplateL = get_template(eyeDetector, eyearea_left, 24);
+                learn_frames++;
+            } else {
+                // Learning finished, use the new templates for template
+                // matching
+                match_eye(eyearea_right, teplateR, method);
+                match_eye(eyearea_left, teplateL, method);
+            }
+             *//*
+
+
         }
+
         return mRgba;
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
+    }
+    private void match_eye(Rect area, Mat mTemplate, int type) {
+        Point matchLoc;
+        Mat mROI = mGray.submat(area);
+        int result_cols = mROI.cols() - mTemplate.cols() + 1;
+        int result_rows = mROI.rows() - mTemplate.rows() + 1;
+        // Check for bad template size
+        if (mTemplate.cols() == 0 || mTemplate.rows() == 0) {
+            return ;
+        }
+        Mat mResult = new Mat(result_cols, result_rows, CvType.CV_8U);
+
+        switch (type) {
+            case TM_SQDIFF:
+                Imgproc.matchTemplate(mROI, mTemplate, mResult, Imgproc.TM_SQDIFF);
+                break;
+            case TM_SQDIFF_NORMED:
+                Imgproc.matchTemplate(mROI, mTemplate, mResult,
+                        Imgproc.TM_SQDIFF_NORMED);
+                break;
+            case TM_CCOEFF:
+                Imgproc.matchTemplate(mROI, mTemplate, mResult, Imgproc.TM_CCOEFF);
+                break;
+            case TM_CCOEFF_NORMED:
+                Imgproc.matchTemplate(mROI, mTemplate, mResult,
+                        Imgproc.TM_CCOEFF_NORMED);
+                break;
+            case TM_CCORR:
+                Imgproc.matchTemplate(mROI, mTemplate, mResult, Imgproc.TM_CCORR);
+                break;
+            case TM_CCORR_NORMED:
+                Imgproc.matchTemplate(mROI, mTemplate, mResult,
+                        Imgproc.TM_CCORR_NORMED);
+                break;
+        }
+
+        Core.MinMaxLocResult mmres = Core.minMaxLoc(mResult);
+        // there is difference in matching methods - best match is max/min value
+        if (type == TM_SQDIFF || type == TM_SQDIFF_NORMED) {
+            matchLoc = mmres.minLoc;
+        } else {
+            matchLoc = mmres.maxLoc;
+        }
+        Point matchLoc_tx = new Point(matchLoc.x + area.x, matchLoc.y + area.y);
+        Point matchLoc_ty = new Point(matchLoc.x + mTemplate.cols() + area.x,
+                matchLoc.y + mTemplate.rows() + area.y);
+
+        Imgproc.rectangle(mRgba, matchLoc_tx, matchLoc_ty, new Scalar(255, 255, 0));
+        Rect rec = new Rect(matchLoc_tx,matchLoc_ty);
+
+    }
+
+    private Mat get_template(CascadeClassifier clasificator, Rect area, int size) {
+        Mat template = new Mat();
+        Mat mROI = mGray.submat(area);
+        MatOfRect eyes = new MatOfRect();
+        Point iris = new Point();
+        Rect eye_template = new Rect();
+        clasificator.detectMultiScale(mROI, eyes, 1.15, 2,
+                Objdetect.CASCADE_FIND_BIGGEST_OBJECT
+                        | Objdetect.CASCADE_SCALE_IMAGE, new Size(30, 30),
+                new Size());
+
+        Rect[] eyesArray = eyes.toArray();
+        for (int i = 0; i < eyesArray.length;) {
+            Rect e = eyesArray[i];
+            e.x = area.x + e.x;
+            e.y = area.y + e.y;
+            Rect eye_only_rectangle = new Rect((int) e.tl().x,
+                    (int) (e.tl().y + e.height * 0.4), (int) e.width,
+                    (int) (e.height * 0.6));
+            mROI = mGray.submat(eye_only_rectangle);
+            Mat vyrez = mRgba.submat(eye_only_rectangle);
+
+
+            Core.MinMaxLocResult mmG = Core.minMaxLoc(mROI);
+
+            Imgproc.circle(vyrez, mmG.minLoc, 2, new Scalar(255, 255, 255), 2);
+            iris.x = mmG.minLoc.x + eye_only_rectangle.x;
+            iris.y = mmG.minLoc.y + eye_only_rectangle.y;
+            eye_template = new Rect((int) iris.x - size / 2, (int) iris.y
+                    - size / 2, size, size);
+            Imgproc.rectangle(mRgba, eye_template.tl(), eye_template.br(),
+                    new Scalar(255, 0, 0), 2);
+            template = (mGray.submat(eye_template)).clone();
+            return template;
+        }
+        return template;
     }
 
     @Override
@@ -328,6 +491,6 @@ public class Convergence extends AppCompatActivity implements CameraBridgeViewBa
             Log.d(TAG, "OpenCV configuration was NOT a success");
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, baseLoaderCallback);
         }
-    }
+    }*/
 
 }
